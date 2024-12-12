b38187de7c93801dee6a990a4934b77aaef7467e
ShaQay3Q <shaghay3gh@gmail.com>
2024-12-12 14:21:08 +0100

    DOM

---
+++ app.js
@@ -0,0 +1,8 @@
+const ul = document.querySelector("ul");
+console.log(ul);
+
+const li = ul.querySelector("li:first-of-type");
+console.log(li);
+
+const newUl = document.querySelector("ul").querySelector("li:last-of-type");
+console.log(newUl);
