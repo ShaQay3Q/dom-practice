e49da882fb501d4c9f47951d5ca73fb1c93dce29
ShaQay3Q <shaghay3gh@gmail.com>
2025-02-19 19:51:26 +0100

    play with DOM

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ console.log(li);
 
 const newUl = document.querySelector("ul").querySelector("li:last-of-type");
 console.log(newUl);
+
+// gets a non-live list - takes only a snap-shot
+const liList = document.querySelectorAll("li");
+
+for (let i = 0; i < liList.length; i++) {
+	console.dir(`li[${i}]: ${liList[i]}`);
+}
+
+// gets a live-list
+const listElements = document.getElementsByTagName("li");
+
+for (let i = 0; i < listElements.length; i++) {
+	console.dir(`li[${i}]: ${listElements[i]}`);
+}
