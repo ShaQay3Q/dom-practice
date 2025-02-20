0f1a40ec6d7790b85924481d20cca6c5513516c2
ShaQay3Q <shaghay3gh@gmail.com>
2025-02-20 12:59:04 +0100

    insert elements on DOM with js

--- app.js
+++ app.js
@@ -21,13 +21,32 @@ for (let i = 0; i < listElements.length; i++) {
 	console.dir(`li[${i}]: ${listElements[i]}`);
 }
 
-const section = document.querySelector("section");
+var section = document.querySelector("section");
 // section.style.backgroundColor = "purple";
 section.className = "";
 section.className = "red-bg";
 
 const btn = document.querySelector("button");
 btn.addEventListener("click", () => {
-	section.classList.toggle("visible");
-	section.classList.toggle("invisible");
+	// section.classList.toggle("visible");
+	// section.classList.toggle("invisible");
+	var newParagraph = document.createElement("p");
+	newParagraph.style.backgroundColor = "yellow";
+	newParagraph.className = "prgph";
+	newParagraph.innerText = "This is an inserted paragraph!";
+	if (section) {
+		section.insertAdjacentElement("beforebegin", newParagraph);
+	} else {
+		console.error("No SECTION have been found!");
+	}
 });
+
+// ---------------------------------------- //
+//		Create & Insert Elements
+// ---------------------------------------- //
+
+// const newParagraph = document.createElement("p");
+// newParagraph.style.backgroundColor = "yellow";
+// newParagraph.className = "prgph";
+// newParagraph.innerText = "This is an inserted paragraph!";
+// section.insertAdjacentElement(before, newParagraph);
